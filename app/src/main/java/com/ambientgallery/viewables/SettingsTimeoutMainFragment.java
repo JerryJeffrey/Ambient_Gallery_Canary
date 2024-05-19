294b7b9b891677992380fee541ec7ad4972437de
Jerry <jerry.jeffrey.1532429240@gmail.com>
2024-05-19 09:46:02 +0800

    Uploaded on 2024.5.19(1)

--- app/src/main/java/com/ambientgallery/viewables/SettingsTimeoutMainFragment.java
+++ app/src/main/java/com/ambientgallery/viewables/SettingsTimeoutMainFragment.java
@@ -1,63 +1,82 @@
 package com.ambientgallery.viewables;
 
+import static com.ambientgallery.utils.SharedPrefsUtil.prefsInt;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.NumberPicker;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.ambientgallery.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class SettingsTimeoutMainFragment extends Fragment {
-    NumberPicker numberPicker0, numberPicker1, numberPicker2;
+    NumberPicker hourPicker, minutePicker, secondPicker;
+    SharedPreferences prefs;
+    TabLayout tabBar;
+    int currentTab = 0;
+    static final int CURRENT = 0, MIN = 1, MAX = 2;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (getActivity() != null)
+            prefs = getActivity().getSharedPreferences("MainPrefs", Context.MODE_PRIVATE);
         return inflater.inflate(R.layout.fragment_settings_timeout_main, container, false);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        numberPicker0 = getActivity().findViewById(R.id.numberPicker000);
-        numberPicker1 = getActivity().findViewById(R.id.numberPicker111);
-        numberPicker2 = getActivity().findViewById(R.id.numberPicker222);
-        setPickerValue(numberPicker0, 0, 24, 0);
-        setPickerValue(numberPicker1, 0, 59, 0);
-        setPickerValue(numberPicker2, 0, 59, 0);
+        hourPicker = getActivity().findViewById(R.id.settings_timeout_main_picker_hour);
+        minutePicker = getActivity().findViewById(R.id.settings_timeout_main_picker_minute);
+        secondPicker = getActivity().findViewById(R.id.settings_timeout_main_picker_second);
+        tabBar = getActivity().findViewById(R.id.settings_timeout_main_tab_container);
+
+        tabBar.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                currentTab = tab.getPosition();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
         NumberPicker.OnValueChangeListener onValueChangeListener = (picker, oldVal, newVal) -> {
             getView().performHapticFeedback(1);
-            if (picker == numberPicker0) {
+            if (picker == hourPicker) {
                 if (newVal == 24) {
-                    numberPicker1.setMaxValue(0);
-                    numberPicker2.setMaxValue(0);
+                    minutePicker.setMaxValue(0);
+                    secondPicker.setMaxValue(0);
                 } else {
-                    numberPicker1.setMaxValue(59);
-                    numberPicker2.setMaxValue(59);
+                    minutePicker.setMaxValue(59);
+                    secondPicker.setMaxValue(59);
                 }
             }
 
-            int n0 = numberPicker0.getValue();
-            int n1 = numberPicker1.getValue();
-            int n2 = numberPicker2.getValue();
+            int n0 = hourPicker.getValue();
+            int n1 = minutePicker.getValue();
+            int n2 = secondPicker.getValue();
             int n = (n0 * 60 + n1) * 60 + n2;
         };
-        numberPicker0.setOnValueChangedListener(onValueChangeListener);
-        numberPicker1.setOnValueChangedListener(onValueChangeListener);
-        numberPicker2.setOnValueChangedListener(onValueChangeListener);
-    }
-
-    private void setPickerValue(NumberPicker picker, int min, int max, int current) {
-        picker.setMaxValue(max);
-        picker.setMinValue(min);
-        picker.setValue(current);
+        hourPicker.setOnValueChangedListener(onValueChangeListener);
+        minutePicker.setOnValueChangedListener(onValueChangeListener);
+        secondPicker.setOnValueChangedListener(onValueChangeListener);
     }
 }
