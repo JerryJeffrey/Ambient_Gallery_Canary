f7a36075aeeca170a99cfea312bb50ca0077f67a
Jerry <jerry.jeffrey.1532429240@gmail.com>
2024-05-24 12:43:54 +0800

    Uploaded on 2024.5.19(1)

--- app/src/main/java/com/ambientgallery/viewables/SettingsTimeoutMainFragment.java
+++ app/src/main/java/com/ambientgallery/viewables/SettingsTimeoutMainFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.NumberPicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -23,7 +24,6 @@ public class SettingsTimeoutMainFragment extends Fragment {
     SharedPreferences prefs;
     TabLayout tabBar;
     int currentTab = 0;
-    static final int CURRENT = 0, MIN = 1, MAX = 2;
 
     @Nullable
     @Override
@@ -40,11 +40,12 @@ public class SettingsTimeoutMainFragment extends Fragment {
         minutePicker = getActivity().findViewById(R.id.settings_timeout_main_picker_minute);
         secondPicker = getActivity().findViewById(R.id.settings_timeout_main_picker_second);
         tabBar = getActivity().findViewById(R.id.settings_timeout_main_tab_container);
-
+        setPickerByTab();
         tabBar.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 currentTab = tab.getPosition();
+                setPickerByTab();
             }
 
             @Override
@@ -60,23 +61,76 @@ public class SettingsTimeoutMainFragment extends Fragment {
 
         NumberPicker.OnValueChangeListener onValueChangeListener = (picker, oldVal, newVal) -> {
             getView().performHapticFeedback(1);
-            if (picker == hourPicker) {
-                if (newVal == 24) {
-                    minutePicker.setMaxValue(0);
-                    secondPicker.setMaxValue(0);
-                } else {
-                    minutePicker.setMaxValue(59);
-                    secondPicker.setMaxValue(59);
-                }
-            }
 
-            int n0 = hourPicker.getValue();
-            int n1 = minutePicker.getValue();
-            int n2 = secondPicker.getValue();
-            int n = (n0 * 60 + n1) * 60 + n2;
+            int currentHour = hourPicker.getValue();
+            int currentMinute = minutePicker.getValue();
+            int currentSecond = secondPicker.getValue();
+            int currentTime = (currentHour * 60 + currentMinute) * 60 + currentSecond;
         };
         hourPicker.setOnValueChangedListener(onValueChangeListener);
         minutePicker.setOnValueChangedListener(onValueChangeListener);
         secondPicker.setOnValueChangedListener(onValueChangeListener);
+    }
+
+    private void setPickerByTab() {
+        int min = 1, max = 86400, current = 1;
+        switch (currentTab) {
+            case 0://hide buttons
+                max = prefsInt(prefs, "ambientTimeout");
+                current = prefsInt(prefs, "hideButtonTimeout");
+                break;
+            case 1://go ambient
+                min = prefsInt(prefs, "hideButtonTimeout");
+                max = prefsInt(prefs, "switchImageTimeout");
+                current = prefsInt(prefs, "ambientTimeout");
+                break;
+            case 2://switch background
+                min = prefsInt(prefs, "ambientTimeout");
+                current = prefsInt(prefs, "switchImageTimeout");
+                break;
+        }
+        setPickerRange(min, max);
+        setPickerCurrent(current);
+
+    }
+
+    private int getPickerCurrent() {
+        return (hourPicker.getValue() * 60 + minutePicker.getValue()) * 60 + secondPicker.getValue();
+    }
+
+    private void setPickerCurrent(int current) {
+        int currentHour = current / 3600;
+        int currentMinute = (current - currentHour * 3600) / 60;
+        int currentSecond = current - currentHour * 3600 - currentMinute * 60;
+        hourPicker.setValue(currentHour);
+        minutePicker.setValue(currentMinute);
+        secondPicker.setValue(currentSecond);
+    }
+
+    private void setPickerRange(int min, int max) {
+        Log.i("asd",min+","+max);
+        int minHour = min / 3600;
+        int minMinute = (min - minHour * 3600) / 60;
+        int minSecond = min - minHour * 3600 - minMinute * 60;
+        int maxHour = max / 3600;
+        int maxMinute = (max - maxHour * 3600) / 60;
+        int maxSecond = max - maxHour * 3600 - maxMinute * 60;
+        hourPicker.setMinValue(minHour);
+        hourPicker.setMaxValue(maxHour);
+        if (maxHour-minHour>0){
+            minutePicker.setMinValue(0);
+            minutePicker.setMaxValue(59);
+        } else {
+            minutePicker.setMinValue(minMinute);
+            minutePicker.setMaxValue(maxMinute);
+        }
+        if (maxMinute-minMinute>0){
+            secondPicker.setMinValue(0);
+            secondPicker.setMaxValue(59);
+        } else {
+            secondPicker.setMinValue(minSecond);
+            secondPicker.setMaxValue(maxSecond);
+        }
+
     }
 }
